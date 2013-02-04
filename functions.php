9b9f00ea2de2bbad8946749f7421f734a06e6e4d
mfssurya <suryap@mindfiresolutions.com>
2013-02-04 17:03:46 +0530

    Added filter to add excerpt more text if excerpt is supplied manually in the post.

--- functions.php
+++ functions.php
@@ -192,6 +192,23 @@ function iribbon_excerpt_link($more) {
 add_filter('excerpt_more', 'iribbon_excerpt_link');
 
 /**
+* Set custom post excerpt link if excerpt is supplied manually.
+*/ 
+function manual_excerpt_read_more_link($output) {
+
+	global $ir_themeslug, $options, $post;
+
+	$linktext = $options->get($ir_themeslug.'_excerpt_link_text');
+	$linktext = $linktext == '' ? 'Read More...' : $linktext;
+	
+	if(!empty($post->post_excerpt))
+		return $output  . '<a href="'. get_permalink($post->ID) . '">'.$linktext.'</a>';
+	else
+		return $output;
+}
+add_filter('the_excerpt', 'manual_excerpt_read_more_link');
+
+/**
 * Set custom post excerpt length based on theme option.
 */ 
 function iribbon_excerpt_length($length) {
