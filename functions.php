86a678a1f15b13e4add4b1061a6f0b89f1972c81
Hugo Saner <hugo@sanerdesign.com>
2013-10-04 11:52:36 +0100

    reformatted code

--- functions.php
+++ functions.php
@@ -17,132 +17,156 @@
 require_once( get_template_directory() . '/cyberchimps/init.php' );
 
 // Set the content width based on the theme's design and stylesheet.
-if ( ! isset( $content_width ) )
-    $content_width = 640; /* pixels */
+if( !isset( $content_width ) ) {
+	$content_width = 640;
+} /* pixels */
 
-function cyberchimps_add_site_info() { ?>
-    <p>&copy; Company Name</p>
-<?php }
-add_action('cyberchimps_site_info', 'cyberchimps_add_site_info');
+function cyberchimps_add_site_info() {
+	?>
+	<p>&copy; Company Name</p>
+<?php
+}
 
-if ( ! function_exists( 'cyberchimps_comment' ) ) :
+add_action( 'cyberchimps_site_info', 'cyberchimps_add_site_info' );
+
+if( !function_exists( 'cyberchimps_comment' ) ) :
 // Template for comments and pingbacks.
 // Used as a callback by wp_list_comments() for displaying the comments.
-    function cyberchimps_comment( $comment, $args, $depth ) {
-        $GLOBALS['comment'] = $comment;
-        switch ( $comment->comment_type ) :
-            case 'pingback' :
-            case 'trackback' :
-                ?>
-                <li class="post pingback">
-                <p><?php _e( 'Pingback:', 'cyberchimps' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'cyberchimps' ), ' ' ); ?></p>
-                <?php
-                break;
-            default :
-                ?>
-                    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-                    <article id="comment-<?php comment_ID(); ?>" class="comment hreview">
-                        <footer>
-                            <div class="comment-author reviewer vcard">
-                                <?php echo get_avatar( $comment, 40 ); ?>
-                                <?php printf( '%1$s <span class="says">%2$s</span>', sprintf( '<cite class="fn">%1$s</cite>',
-                                                                                              get_comment_author_link() ),
-                                              __( 'says', 'cyberchimps' ) ); ?>
-                            </div><!-- .comment-author .vcard -->
-                            <?php if ( $comment->comment_approved == '0' ) : ?>
-                                <em><?php _e( 'Your comment is awaiting moderation.', 'cyberchimps' ); ?></em>
-                                <br />
-                            <?php endif; ?>
+	function cyberchimps_comment( $comment, $args, $depth ) {
+		$GLOBALS['comment'] = $comment;
+		switch( $comment->comment_type ) :
+			case 'pingback' :
+			case 'trackback' :
+				?>
+				<li class="post pingback">
+				<p><?php _e( 'Pingback:', 'cyberchimps' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'cyberchimps' ), ' ' ); ?></p>
+				<?php
+				break;
+			default :
+				?>
+					<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+					<article id="comment-<?php comment_ID(); ?>" class="comment hreview">
+						<footer>
+							<div class="comment-author reviewer vcard">
+								<?php echo get_avatar( $comment, 40 ); ?>
+								<?php printf( '%1$s <span class="says">%2$s</span>', sprintf( '<cite class="fn">%1$s</cite>',
+								                                                              get_comment_author_link() ),
+								              __( 'says', 'cyberchimps' ) ); ?>
+							</div>
+							<!-- .comment-author .vcard -->
+							<?php if( $comment->comment_approved == '0' ) : ?>
+								<em><?php _e( 'Your comment is awaiting moderation.', 'cyberchimps' ); ?></em>
+								<br/>
+							<?php endif; ?>
 
-                            <div class="comment-meta commentmetadata">
-                                <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>" class="dtreviewed"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
-                                        <?php
-                                        /* translators: 1: date, 2: time */
-                                        printf( __( '%1$s at %2$s', 'cyberchimps' ), get_comment_date(), get_comment_time() ); ?>
-                                    </time></a>
-                                <?php edit_comment_link( __( '(Edit)', 'cyberchimps' ), ' ' );
-                                ?>
-                            </div><!-- .comment-meta .commentmetadata -->
-                        </footer>
+							<div class="comment-meta commentmetadata">
+								<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>" class="dtreviewed">
+									<time pubdate datetime="<?php comment_time( 'c' ); ?>">
+										<?php
+										/* translators: 1: date, 2: time */
+										printf( __( '%1$s at %2$s', 'cyberchimps' ), get_comment_date(), get_comment_time() ); ?>
+									</time>
+								</a>
+								<?php edit_comment_link( __( '(Edit)', 'cyberchimps' ), ' ' );
+								?>
+							</div>
+							<!-- .comment-meta .commentmetadata -->
+						</footer>
 
-                        <div class="comment-content"><?php comment_text(); ?></div>
+						<div class="comment-content"><?php comment_text(); ?></div>
 
-                        <div class="reply">
-                            <?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-                        </div><!-- .reply -->
-                    </article><!-- #comment-## -->
+						<div class="reply">
+							<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+						</div>
+						<!-- .reply -->
+					</article><!-- #comment-## -->
 
-                <?php
-                break;
-        endswitch;
-    }
+				<?php
+				break;
+		endswitch;
+	}
 endif; // ends check for cyberchimps_comment()
 
 // set up next and previous post links for lite themes only
 function cyberchimps_next_previous_posts() {
-    if( get_next_posts_link() || get_previous_posts_link() ): ?>
-        <div class="more-content">
-            <div class="row-fluid">
-                <div class="span6 previous-post">
-                    <?php previous_posts_link(); ?>
-                </div>
-                <div class="span6 next-post">
-                    <?php next_posts_link(); ?>
-                </div>
-            </div>
-        </div>
-    <?php
-    endif;
+	if( get_next_posts_link() || get_previous_posts_link() ): ?>
+		<div class="more-content">
+			<div class="row-fluid">
+				<div class="span6 previous-post">
+					<?php previous_posts_link(); ?>
+				</div>
+				<div class="span6 next-post">
+					<?php next_posts_link(); ?>
+				</div>
+			</div>
+		</div>
+	<?php
+	endif;
 }
+
 add_action( 'cyberchimps_after_content', 'cyberchimps_next_previous_posts' );
 
-if ( ! function_exists( 'cyberchimps_theme_check' ) ) :
+if( !function_exists( 'cyberchimps_theme_check' ) ) :
 // core options customization Names and URL's
 //Pro or Free has to stay prepended with cyberchimps_
-    function cyberchimps_theme_check() {
-        $level = 'free';
-        return $level;
-    }
+	function cyberchimps_theme_check() {
+		$level = 'free';
+
+		return $level;
+	}
 endif;
 //Theme Name
-function cyberchimps_options_theme_name(){
-    $text = 'iRibbon';
-    return $text;
+function cyberchimps_options_theme_name() {
+	$text = 'iRibbon';
+
+	return $text;
 }
+
 //Doc's URL
 function cyberchimps_options_documentation_url() {
-    $url = 'http://cyberchimps.com/guides/c-free/';
-    return $url;
+	$url = 'http://cyberchimps.com/guides/c-free/';
+
+	return $url;
 }
+
 // Support Forum URL
 function cyberchimps_options_support_forum() {
-    $url = 'http://cyberchimps.com/forum/free/iribbon-free/';
-    return $url;
+	$url = 'http://cyberchimps.com/forum/free/iribbon-free/';
+
+	return $url;
 }
+
 add_filter( 'cyberchimps_current_theme_name', 'cyberchimps_options_theme_name', 1 );
 add_filter( 'cyberchimps_documentation', 'cyberchimps_options_documentation_url' );
 add_filter( 'cyberchimps_support_forum', 'cyberchimps_options_support_forum' );
 
 //upgrade bar
-function cyberchimps_upgrade_bar_pro_title(){
-    $title = 'iRibbon Pro 2';
-    return $title;
+function cyberchimps_upgrade_bar_pro_title() {
+	$title = 'iRibbon Pro 2';
+
+	return $title;
 }
-function cyberchimps_upgrade_link(){
-    $link = 'http://cyberchimps.com/store/iribbon-pro/';
-    return $link;
+
+function cyberchimps_upgrade_link() {
+	$link = 'http://cyberchimps.com/store/iribbon-pro/';
+
+	return $link;
 }
+
 add_filter( 'cyberchimps_upgrade_pro_title', 'cyberchimps_upgrade_bar_pro_title' );
 add_filter( 'cyberchimps_upgrade_link', 'cyberchimps_upgrade_link' );
 
 // Help Section
 function cyberchimps_options_help_header() {
-    $text = 'iRibbon';
-    return $text;
+	$text = 'iRibbon';
+
+	return $text;
 }
-function cyberchimps_options_help_sub_header(){
-    $text = __( 'iRibbon an elegant and responsive WordPress Theme', 'cyberchimps' );
-    return $text;
+
+function cyberchimps_options_help_sub_header() {
+	$text = __( 'iRibbon an elegant and responsive WordPress Theme', 'cyberchimps' );
+
+	return $text;
 }
 
 add_filter( 'cyberchimps_help_heading', 'cyberchimps_options_help_header' );
@@ -152,71 +176,81 @@ add_filter( 'cyberchimps_help_sub_heading', 'cyberchimps_options_help_sub_header
 
 // Banner default
 function cyberchimps_banner_default() {
-    $url = '/images/branding/banner.jpg';
-    return $url;
+	$url = '/images/branding/banner.jpg';
+
+	return $url;
 }
+
 add_filter( 'cyberchimps_banner_img', 'cyberchimps_banner_default' );
-
-
 
 //theme specific skin options in array. Must always include option default
 function cyberchimps_skin_color_options( $options ) {
-    // Get path of image
-    $imagepath = get_template_directory_uri(). '/inc/css/skins/images/';
+	// Get path of image
+	$imagepath = get_template_directory_uri() . '/inc/css/skins/images/';
 
-    $options = array(
-        'default'	=> $imagepath . 'default.png'
-    );
-    return $options;
+	$options = array(
+		'default' => $imagepath . 'default.png'
+	);
+
+	return $options;
 }
+
 add_filter( 'cyberchimps_skin_color', 'cyberchimps_skin_color_options', 1 );
 
 // theme specific background images
 function cyberchimps_background_image( $options ) {
-    $imagepath =  get_template_directory_uri() . '/cyberchimps/lib/images/';
-    $options = array(
-        'none' => $imagepath . 'backgrounds/thumbs/none.png',
-        'noise' => $imagepath . 'backgrounds/thumbs/noise.png',
-        'blue' => $imagepath . 'backgrounds/thumbs/blue.png',
-        'dark' => $imagepath . 'backgrounds/thumbs/dark.png',
-        'space' => $imagepath . 'backgrounds/thumbs/space.png'
-    );
-    return $options;
+	$imagepath = get_template_directory_uri() . '/cyberchimps/lib/images/';
+	$options   = array(
+		'none'  => $imagepath . 'backgrounds/thumbs/none.png',
+		'noise' => $imagepath . 'backgrounds/thumbs/noise.png',
+		'blue'  => $imagepath . 'backgrounds/thumbs/blue.png',
+		'dark'  => $imagepath . 'backgrounds/thumbs/dark.png',
+		'space' => $imagepath . 'backgrounds/thumbs/space.png'
+	);
+
+	return $options;
 }
+
 add_filter( 'cyberchimps_background_image', 'cyberchimps_background_image' );
 
 // theme specific typography options
 function cyberchimps_typography_sizes( $sizes ) {
-    $sizes = array( '8','9','10','12','14','16','20' );
-    return $sizes;
+	$sizes = array( '8', '9', '10', '12', '14', '16', '20' );
+
+	return $sizes;
 }
+
 function cyberchimps_typography_faces( $faces ) {
-    $faces = array(
-        'Arial, Helvetica, sans-serif'						 => 'Arial',
-        'Arial Black, Gadget, sans-serif'					 => 'Arial Black',
-        'Comic Sans MS, cursive'							 => 'Comic Sans MS',
-        'Courier New, monospace'							 => 'Courier New',
-        'Georgia, serif'									 => 'Georgia',
-        'Impact, Charcoal, sans-serif'						 => 'Impact',
-        'Lucida Console, Monaco, monospace'					 => 'Lucida Console',
-        'Lucida Sans Unicode, Lucida Grande, sans-serif'	 => 'Lucida Sans Unicode',
-        'Palatino Linotype, Book Antiqua, Palatino, serif'	 => 'Palatino Linotype',
-        'Tahoma, Geneva, sans-serif'						 => 'Tahoma',
-        'Times New Roman, Times, serif'						 => 'Times New Roman',
-        'Trebuchet MS, sans-serif'							 => 'Trebuchet MS',
-        'Verdana, Geneva, sans-serif'						 => 'Verdana',
-        'Symbol'											 => 'Symbol',
-        'Webdings'											 => 'Webdings',
-        'Wingdings, Zapf Dingbats'							 => 'Wingdings',
-        'MS Sans Serif, Geneva, sans-serif'					 => 'MS Sans Serif',
-        'MS Serif, New York, serif'							 => 'MS Serif',
-    );
-    return $faces;
+	$faces = array(
+		'Arial, Helvetica, sans-serif'                     => 'Arial',
+		'Arial Black, Gadget, sans-serif'                  => 'Arial Black',
+		'Comic Sans MS, cursive'                           => 'Comic Sans MS',
+		'Courier New, monospace'                           => 'Courier New',
+		'Georgia, serif'                                   => 'Georgia',
+		'Impact, Charcoal, sans-serif'                     => 'Impact',
+		'Lucida Console, Monaco, monospace'                => 'Lucida Console',
+		'Lucida Sans Unicode, Lucida Grande, sans-serif'   => 'Lucida Sans Unicode',
+		'Palatino Linotype, Book Antiqua, Palatino, serif' => 'Palatino Linotype',
+		'Tahoma, Geneva, sans-serif'                       => 'Tahoma',
+		'Times New Roman, Times, serif'                    => 'Times New Roman',
+		'Trebuchet MS, sans-serif'                         => 'Trebuchet MS',
+		'Verdana, Geneva, sans-serif'                      => 'Verdana',
+		'Symbol'                                           => 'Symbol',
+		'Webdings'                                         => 'Webdings',
+		'Wingdings, Zapf Dingbats'                         => 'Wingdings',
+		'MS Sans Serif, Geneva, sans-serif'                => 'MS Sans Serif',
+		'MS Serif, New York, serif'                        => 'MS Serif',
+	);
+
+	return $faces;
 }
+
 function cyberchimps_typography_styles( $styles ) {
-    $styles = array( 'normal' => 'Normal','bold' => 'Bold' );
-    return $styles;
+	$styles = array( 'normal' => 'Normal', 'bold' => 'Bold' );
+
+	return $styles;
 }
+
 add_filter( 'cyberchimps_typography_sizes', 'cyberchimps_typography_sizes' );
 add_filter( 'cyberchimps_typography_faces', 'cyberchimps_typography_faces' );
 add_filter( 'cyberchimps_typography_styles', 'cyberchimps_typography_styles' );
@@ -229,9 +263,9 @@ add_filter( 'cyberchimps_typography_styles', 'cyberchimps_typography_styles' );
  * @return string
  */
 function cyberchimps_sidebar_before_widget_title( $title ) {
-    $title = '<div class="cc-widget-title-container">' . $title;
+	$title = '<div class="cc-widget-title-container">' . $title;
 
-    return $title;
+	return $title;
 }
 
 add_action( 'cyberchimps_sidebar_before_widget_title', 'cyberchimps_sidebar_before_widget_title', 10, 1 );
@@ -244,16 +278,16 @@ add_action( 'cyberchimps_sidebar_before_widget_title', 'cyberchimps_sidebar_befo
  * @return string
  */
 function cyberchimps_sidebar_after_widget_title( $title ) {
-    $title = $title . '</div>';
+	$title = $title . '</div>';
 
-    return $title;
+	return $title;
 }
 
 add_action( 'cyberchimps_sidebar_after_widget_title', 'cyberchimps_sidebar_after_widget_title', 10, 1 );
 
 // Set separator for post entry meta.
 function cyberchimps_entry_meta_sep() {
-    return '.';
+	return '.';
 }
 
 add_filter( 'cyberchimps_entry_meta_sep', 'cyberchimps_entry_meta_sep' );
@@ -261,11 +295,11 @@ add_filter( 'cyberchimps_entry_meta_sep', 'cyberchimps_entry_meta_sep' );
 // Customize widget element before_title markup.
 function cyberchimps_before_widget_title( $title ) {
 
-    $before_title = '<div class="cc-widget-title-container">';
+	$before_title = '<div class="cc-widget-title-container">';
 
-    $title = $before_title . $title;
+	$title = $before_title . $title;
 
-    return $title;
+	return $title;
 }
 
 add_filter( 'cyberchimps_before_widget_title', 'cyberchimps_before_widget_title', 10, 1 );
@@ -273,21 +307,21 @@ add_filter( 'cyberchimps_before_widget_title', 'cyberchimps_before_widget_title'
 // Customize widget element after_title markup.
 function cyberchimps_after_widget_title( $title ) {
 
-    $after_title = '</div>';
+	$after_title = '</div>';
 
-    $title = $title . $after_title;
+	$title = $title . $after_title;
 
-    return $title;
+	return $title;
 }
 
 add_filter( 'cyberchimps_after_widget_title', 'cyberchimps_after_widget_title', 10, 1 );
 
 function cyberchimps_after_widget( $after ) {
-    $after_widget = '<div class="ribbon-bottom-container"><div class="ribbon-bottom"></div></div>';
+	$after_widget = '<div class="ribbon-bottom-container"><div class="ribbon-bottom"></div></div>';
 
-    $after = $after_widget . $after;
+	$after = $after_widget . $after;
 
-    return $after;
+	return $after;
 }
 
 add_action( 'cyberchimps_after_widget', 'cyberchimps_after_widget', 10, 1 );
@@ -295,10 +329,10 @@ add_action( 'cyberchimps_after_widget', 'cyberchimps_after_widget', 10, 1 );
 // Add goggle font Lobster.
 function cyberchimps_add_google_font() {
 
-    // Check if SSL is present, if so then use https othereise use http
-    $protocol = is_ssl() ? 'https' : 'http';
-    ?>
-    <link rel='stylesheet' href='<?php echo $protocol; ?>://fonts.googleapis.com/css?family=Lobster' type='text/css'>
+	// Check if SSL is present, if so then use https othereise use http
+	$protocol = is_ssl() ? 'https' : 'http';
+	?>
+	<link rel='stylesheet' href='<?php echo $protocol; ?>://fonts.googleapis.com/css?family=Lobster' type='text/css'>
 <?php
 }
 
--- no-results.php
+++ no-results.php
@@ -14,31 +14,34 @@
  */
 ?>
 
-    <div class="ribbon-top">
-        <h1 class="entry-title"><?php _e( 'Nothing Found', 'cyberchimps' ); ?></h1>
-    </div>
+<div class="ribbon-top">
+	<h1 class="entry-title"><?php _e( 'Nothing Found', 'cyberchimps' ); ?></h1>
+</div>
 
 <article id="post-0" class="post no-results not-found" xmlns="http://www.w3.org/1999/html">
 
 	<div class="entry-content">
-		<?php if ( is_home() ) { ?>
+		<?php if( is_home() ) { ?>
 
-			<p><?php printf( '%1$s <a href="%2$s"></a>.', 
-												__( 'Ready to publish your first post?', 'cyberchimps' ),
-												admin_url( 'post-new.php' ),
-                        __( 'Get started here', 'cyberchimps' )
-											); ?></p>
+			<p><?php printf( '%1$s <a href="%2$s"></a>.',
+			                 __( 'Ready to publish your first post?', 'cyberchimps' ),
+			                 admin_url( 'post-new.php' ),
+			                 __( 'Get started here', 'cyberchimps' )
+				); ?></p>
 
-		<?php } elseif ( is_search() ) { ?>
+		<?php }
+		elseif( is_search() ) { ?>
 
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'cyberchimps' ); ?></p>
 			<?php get_search_form(); ?>
 
-		<?php } else { ?>
+		<?php }
+		else { ?>
 
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'cyberchimps' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php } ?>
-	</div><!-- .entry-content -->
+	</div>
+	<!-- .entry-content -->
 </article><!-- #post-0 -->
