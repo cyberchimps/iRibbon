9a0cf5443aad8bc5fbc15ca26479e2e0c82f277e
pallavi <pallavi.chanodia@hbwsl.com>
2018-02-28 11:25:23 +0530

    new fixed issue with default footer widget layout

--- footer.php
+++ footer.php
@@ -21,32 +21,45 @@ if( cyberchimps_get_option( 'footer_show_toggle' ) == '1' ) {
 		<div class="container">
 			<div class="container-fluid">
 				<div id="footer-widgets" class="row-fluid">
-					<div id="footer-widget-container" class="span12">
-						<div class="row-fluid">
+					<div id="footer-widget-container" class="span12">						
 
 							<?php if( !dynamic_sidebar( 'cyberchimps-footer-widgets' ) ) : ?>
-								<aside class="widget-container span3">
+							
+							<?php 
+								$footer_widget_layout = cyberchimps_get_option('site_footer_option');
+								if(isset($footer_widget_layout) && $footer_widget_layout != '')
+									$layout = $footer_widget_layout;
+								else
+								$layout = '';
+								
+								if ($layout == 'footer-3-col')
+								{
+									$cls = 'span4';
+								?>
+								<div class="row-fluid">
+								<aside class="widget-container <?php echo $cls;?>">
 									<h3 class="widget-title"><?php _e( 'Pages', 'iribbon' ); ?></h3>
 									<ul>
 										<?php wp_list_pages( 'title_li=' ); ?>
 									</ul>
 								</aside>
 
-								<aside class="widget-container span3">
+								<aside class="widget-container <?php echo $cls;?>">
 									<h3 class="widget-title"><?php _e( 'Archives', 'iribbon' ); ?></h3>
 									<ul>
 										<?php wp_get_archives( 'type=monthly' ); ?>
 									</ul>
 								</aside>
 
-								<aside class="widget-container span3">
+								<aside class="widget-container <?php echo $cls;?>">
 									<h3 class="widget-title"><?php _e( 'Categories', 'iribbon' ); ?></h3>
 									<ul>
 										<?php wp_list_categories( 'show_count=1&title_li=' ); ?>
 									</ul>
 								</aside>
-
-								<aside class="widget-container span3">
+								</div>
+								<div class="row-fluid">
+								<aside class="widget-container <?php echo $cls;?>">
 									<h3 class="widget-title"><?php _e( 'WordPress', 'iribbon' ); ?></h3>
 									<ul>
 										<?php wp_register(); ?>
@@ -57,9 +70,51 @@ if( cyberchimps_get_option( 'footer_show_toggle' ) == '1' ) {
 										<?php wp_meta(); ?>
 									</ul>
 								</aside>
+								</div>
+								<?php 	
+								}
+								
+								else {
+									$cls = 'span3 ';
+							?>
+							<div class="row-fluid">
+								<aside class="widget-container <?php echo $cls;?>">
+									<h3 class="widget-title"><?php _e( 'Pages', 'iribbon' ); ?></h3>
+									<ul>
+										<?php wp_list_pages( 'title_li=' ); ?>
+									</ul>
+								</aside>
+
+								<aside class="widget-container <?php echo $cls;?>">
+									<h3 class="widget-title"><?php _e( 'Archives', 'iribbon' ); ?></h3>
+									<ul>
+										<?php wp_get_archives( 'type=monthly' ); ?>
+									</ul>
+								</aside>
+
+								<aside class="widget-container <?php echo $cls;?>">
+									<h3 class="widget-title"><?php _e( 'Categories', 'iribbon' ); ?></h3>
+									<ul>
+										<?php wp_list_categories( 'show_count=1&title_li=' ); ?>
+									</ul>
+								</aside>
+
+								<aside class="widget-container <?php echo $cls;?>">
+									<h3 class="widget-title"><?php _e( 'WordPress', 'iribbon' ); ?></h3>
+									<ul>
+										<?php wp_register(); ?>
+										<li><?php wp_loginout(); ?></li>
+										<li><a href="<?php echo esc_url( 'http://wordpress.org/' ); ?>" target="_blank"
+										       title="<?php esc_attr_e( 'Powered by WordPress, state-of-the-art semantic personal publishing platform.', 'iribbon' ); ?>"> <?php _e( 'WordPress', 'iribbon' ); ?></a>
+										</li>
+										<?php wp_meta(); ?>
+									</ul>
+								</aside>
+									</div>
+							<!-- .row-fluid -->
+								<?php }?>
 							<?php endif; ?>
-						</div>
-						<!-- .row-fluid -->
+					
 					</div>
 					<!-- #footer-widget-container -->
 				</div>
